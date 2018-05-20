ce736163755469012f6c824a24f395a4fec9d3d6
David Moreira <david_nogueira_moreira@hotmail.com>
2018-05-20 16:07:37 +0100

    Portfolio update.
    A refresh to my portfolio.

--- portfolio/js/scripts.js
+++ portfolio/js/scripts.js
@@ -19,7 +19,6 @@ this.screenshotPreview = function () {
             .css("position", "absolute")
             .css("top", (e.pageY - xOffset) + "px")
             .css("left", (e.pageX + yOffset) + "px")
-
             .fadeIn("fast");
     },
         function () {
@@ -36,39 +35,60 @@ this.screenshotPreview = function () {
 
 
 
-$(document).ready(function () {
-    screenshotPreview();
-    var on = true;
-    $(".language-toggle").on("click", function () {
+this.addEventListener("DOMContentLoaded", function () {
+    currentDate = new Date();
+    document.getElementById("currentYear").textContent = currentDate.getFullYear();
+
+    //Smooth Scrolling
+    document.querySelectorAll('a[href^="#"].scroll-down').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            console.log(this);
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                block: "center",
+                inline: "nearest"
+            });
+        });
+    });
+    
+    let on = true;
+    let langToggle = document.querySelector(".language-toggle");
+
+    langToggle.addEventListener("click", () =>
+    {
+        let langDropDown = document.querySelector(".language-dropdown");
         if (on) {
-            $(".language-dropdown").css("display", "block");
-            $(".language-dropdown").animate({ width: '350px', opacity: '100%' }, "fast");
-
-
-
+            langDropDown.style["display"] = "block";
+            $(langDropDown).animate({ width: '350px', opacity: '100%' }, "fast");
             on = false;
-
         }
         else {
-            $(".language-dropdown").animate({ width: '0px', opacity: '100%' }, "fast");
-
-
+            $(langDropDown).animate({ width: '0px', opacity: '100%' }, "fast");
             setTimeout(function () {
-                $(".language-dropdown").css("display", "none");
+                langDropDown.style["display"] = "none";
             }, 150);
-
             on = true;
         }
     });
 
     //Make active navbar buttons on Click
-    $("#centerNav a").on("click", function () {
-        $("#centerNav a").removeClass("active");
-        $(this).addClass("active");
-    });
+    let navBtns = document.querySelectorAll("#centerNav a");
+    navBtns.forEach(btn => 
+        { 
+            btn.addEventListener("click", function (e) 
+            {
+                navBtns.forEach(btnClass =>
+                {
+                    btnClass.classList.remove("active");
+                });
+                this.classList.add("active");
+            });
+        });
 
     //Scroll
-    $(document).on("scroll", function () {
+    this.addEventListener("scroll", () =>
+    {
         var posY = (window.pageYOffset);
         var active = "#aAbout";
         if (posY > $("#skills").offset().top - 100) {
@@ -81,6 +101,57 @@ $(document).ready(function () {
         $(active).addClass("active");
     });
 
+    //Match Height on row columns
+    $(function() {
+        $('.panel').matchHeight();
+        $('.portfolio-text').matchHeight();
+    });
+
+
+    //Microsoft Certified Animation
+window.addEventListener('load',() => {
+    const canvasEl = document.querySelector('canvas')
+    const ctx = canvasEl.getContext('2d')
+    const brushWidth = 100
+    const fontSize = '30px'
+    const speed = 10
+    const txt = 'Microsoft Certified!'
+    
+    let brushOffset = brushWidth
+    let x = 10
+    let i = 0
+    
+    // Font settings
+    ctx.font = `${fontSize} Roboto`
+    
+    ctx.lineWidth = 2
+    
+    ctx.fillStyle = '#212121'
+    
+    const draw = () => {
+      ctx.clearRect(x, 100, 400, 150);
+      ctx.setLineDash([brushWidth, brushOffset, brushOffset - speed]);
+      brushOffset -= speed;
+      ctx.strokeText(txt[i], x, 50);
+    
+      if (brushOffset > 0) {
+        requestAnimationFrame(draw);
+      } else {
+        brushOffset = brushWidth
+        x += ctx.measureText(txt[i++]).width + ctx.lineWidth * Math.random();
+  
+        if (i < txt.length) {
+          requestAnimationFrame(draw);
+        }
+      }
+    } 
+    draw();
+  });
+  
 });
+
+
+
+
 
 
